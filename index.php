7b7006dce2dcfb7d6fe0b1b277b7a3f8ae4e1c1d
Azten6363 <adrianpg@protonmail.com>
2024-08-25 23:17:31 -0600

    Agregado barra de navegación y función Insert para el CRUD

--- index.php
+++ index.php
@@ -8,34 +8,95 @@
     <script src="https://kit.fontawesome.com/fc90ac564b.js" crossorigin="anonymous"></script>
 </head>
 <body>
+<header>
+      <nav class="navbar navbar-expand-md fixed-top navbar-light bg-primary"> <div class="container">
+
+      <a href="#" class="navbar-brand mb-0 h1" >  <img class="d-inline-block align-top" src="imagenes/logo sistemas.png" alt="Descripción de la imagen" width="40" height="40"> ISC
+
+      </a>
+      <button 
+      class="navbar-toggler" 
+      type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+      <div class = "collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a href="#" class="nav-link">
+            Home
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Features
+          </a>
+          <ul class="dropdown-menu"
+          aria-labelledby="navbarDropdown">
+          <li>
+            <a href="#" class="dropdown-item">hola1</a>
+          </li>
+          <li>
+            <a href="#" class="dropdown-item">hola2</a>
+          </li>
+          <li>
+            <a href="#" class="dropdown-item">hola3</a>
+          </li>
+          </ul>
+        </li>
+        <li class="nav-item active">
+          <a href="#" class="nav-link">
+            Pricing
+          </a>
+        </li>
+      </ul>
+
+        
+      </div>
+    </div>
+    </nav>
+    </header>
+    
     <h1 class="text-center p-3">Plantilla crud</h1>
     <div class="container-fluid row">
-
-    <form class ="col-4 p-3">
-  <h3 class="text-center text-secondary">Registro de personas</h3>
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Nombre de la persona</label>
-    <input type="text" class="form-control" name="nombre">
-      </div>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Apellido de la persona</label>
-    <input type="text" class="form-control" name="apellido">
-      </div>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Dni de la persona</label>
-    <input type="text" class="form-control" name="dnie">
-      </div>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento </label>
-    <input type="date" class="form-control" name="fecha">
-      </div>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Correo</label>
-    <input type="email" class="form-control" name="correo">
-      </div>
+      <form class ="col-4 p-3" method="POST">
+        <h3 class="text-center text-secondary">Registro de personas</h3>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Nombres de la persona</label>
+          <input type="text" class="form-control" name="nombres">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Apellidos de la persona</labe>
+          <input type="text" class="form-control" name="apellidos">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Dni de la persona</label>
+          <input type="text" class="form-control" name="dni">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento </label>
+          <input type="date" class="form-control" name="fecha">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Correo</label>
+          <input type="email" class="form-control" name="correo">
+        </div>
   
-  <button type="submit" class="btn btn-primary"name="btnregistrar" value="ok" >Registrar</button>
-</form>
+        <button type="submit" class="btn btn-primary"name="btnregistrar" value="ok" >Registrar</button>
+
+        <div class="mb-3" >
+          <?php
+          include "modelo/conexion.php";
+          include "controlador/registro_persona.php";
+          ?>
+        </div>
+      </form>
 <div class="col-8 p-4">
 
 <table class="table">
@@ -51,6 +112,7 @@
     </tr>
   </thead>
   <tbody>
+  
     <?php
 include "modelo/conexion.php";
 $sql = $conexion->query("select * from persona");
@@ -72,11 +134,8 @@ while ($datos = $sql->fetch_object()) { ?>
       </td>
 <?php }
     ?>
-   
-   
   </tbody>
 </table>
-
 </div>
 </div>
 
